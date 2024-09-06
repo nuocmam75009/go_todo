e1036daab5b7ce8289d35d18caddf2a78dc0bd11
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-09-06 17:21:27 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"context"
+  "fmt"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/thedevsaddam/renderer"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
